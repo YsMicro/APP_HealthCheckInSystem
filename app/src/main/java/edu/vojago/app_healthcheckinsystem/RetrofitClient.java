dda1d461db449759800e896e8217d8d90c5f9ed4
Vojago <ywh2742450799@gmail.com>
2025-05-20 12:12:20 +0800

    feat(network): 更新网络配置并添加新地址
    - 在 network_security_config.xml 中添加新的域名配置
    - 更新 RetrofitClient 中的 BASE_URL地址
    - 注释掉本地测试地址

--- app/src/main/java/edu/vojago/app_healthcheckinsystem/RetrofitClient.java
+++ app/src/main/java/edu/vojago/app_healthcheckinsystem/RetrofitClient.java
@@ -5,7 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     // 修改为实际后端地址（示例）
-    private static final String BASE_URL = "http://8.140.238.228:8080/";
+    private static final String BASE_URL = "http://8.140.238.228:8080";
+    //    private static final String BASE_URL = "http://192.168.38.238:8080";
     private static Retrofit retrofit;
 
     public static Retrofit getInstance() {
